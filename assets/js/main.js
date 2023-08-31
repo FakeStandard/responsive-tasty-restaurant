c2ee75e7dc736ba762941a9f421226e08dc2835e
riley.lin <rileylin.space@gmail.com>
2023-08-31 18:00:45 +0800

    add scroll top icon

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,7 +32,45 @@ navLink.forEach(f => f.addEventListener('click', linkAction))
 const sections = document.querySelectorAll('section[id')
 
 function scrollActive() {
-    //
+    const scrollY = window.pageYOffset
+
+    sections.forEach(f => {
+        const height = f.offsetHeigth
+        const top = f.offsetTop - 50
+
+        sectionId = f.getAttribute('id')
+
+        if (scrollY > top && scrollY <= top + height) {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
 }
 
 window.addEventListener('scroll', scrollActive)
+
+/* Change Background Header */
+function scrollHeader() {
+    const nav = document.getElementById('header')
+
+    if (this.scrollY >= 200) {
+        nav.classList.add('scroll-header')
+    } else {
+        nav.classList.remove('scroll-header')
+    }
+}
+
+window.addEventListener('scroll', scrollHeader)
+
+/* Show Scroll Top */
+function scrollTop() {
+    const scrollTop = document.getElementById('scroll-top')
+    if (this.scrollY >= 560) {
+        scrollTop.classList.add('show-scroll')
+    } else {
+        scrollTop.classList.remove('show-scroll')
+    }
+}
+
+window.addEventListener('scroll', scrollTop)
